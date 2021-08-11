abee08e0489d2d9e04ab28f6289e807076dda24d
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-08-11 04:20:08 +0000

    installed crispy-forms

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,9 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
+AUTH_USER_MODEL = "account.User"
+
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -41,7 +44,10 @@ INSTALLED_APPS = [
     
     'finelib_scrapper',
     'account',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,3 +133,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = "index"
+LOGOUT_REDIRECT_URL = "login"
+LOGIN_URL = "login"
