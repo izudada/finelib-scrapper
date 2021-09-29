679704ead3fd09255bb718b9ebb965779d3a5256
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-09-29 08:53:44 +0000

    Add index, and auth urls

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,8 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from . import views 
 
 urlpatterns = [
-
+    path('account/register/', views.UserCreateView.as_view(), name='register'),
+    path('',  views.IndexView.as_view(), name='index' ),
+    path('account/', include('django.contrib.auth.urls')),
 ]
