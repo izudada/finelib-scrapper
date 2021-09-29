93188b83d606da3d85395686854f0212f6f5af3b
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-09-29 09:00:01 +0000

    Creat UserCreateView and IndexView

--- account/views.py
+++ account/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
+from django.urls import  reverse_lazy, reverse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.views.generic import TemplateView, CreateView
+from .forms import LoginForm, RegisterForm
+from .models import User
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class UserCreateView(CreateView):
+    model = User
+    form_class = RegisterForm
+    success_url = reverse_lazy('login')
+    template_name = 'register.html'
+
+    def form_valid(self, form):
+        return super(UserCreateView, self).form_valid(form)
