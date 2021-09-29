18fa2304422d03a7ec29523f009573644c73962a
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-09-29 08:50:41 +0000

    Add dashboard url

--- finelib_scrapper/urls.py
+++ finelib_scrapper/urls.py
@@ -1,6 +1,6 @@
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from . import views 
 
 urlpatterns = [
-
+    path('dashboard/', views.Dashboard.as_view(), name='dashboard'),
 ]
