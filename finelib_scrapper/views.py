49d0ffa8ae38f058af5f39cad7e53d1bb43f6b3d
Udeagbala Anthony <tonyudeagbala@gmail.com>
2021-09-29 09:04:26 +0000

    Create IndexView

--- finelib_scrapper/views.py
+++ finelib_scrapper/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
+from django.urls import  reverse_lazy, reverse
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import TemplateView
+from account.models import User
 
-# Create your views here.
+
+class Dashboard(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard.html'
